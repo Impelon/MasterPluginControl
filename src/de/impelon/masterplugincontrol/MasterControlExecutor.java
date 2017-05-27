d4408f2ea554b00bb904adb5a593594026c26118
Ovi T. (Impelon) <ovi.tatar@googlemail.com>
2017-05-27 10:20:10 +0200

    1.0.2 update
    - mostly internal changes for better code structure
    - the helppages are now displayed on multiple pages

--- src/de/impelon/masterplugincontrol/MasterControlExecutor.java
+++ src/de/impelon/masterplugincontrol/MasterControlExecutor.java
@@ -1,4 +1,4 @@
-package de.Impelon.masterplugincontrol;
+package de.impelon.masterplugincontrol;
 
 import java.io.File;
 
@@ -12,12 +12,6 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class MasterControlExecutor implements CommandExecutor {
 
-	private MasterControlMain main;
-
-	public MasterControlExecutor(MasterControlMain main) {
-		this.main = main;
-	}
-
 	@Override
 	public boolean onCommand(final CommandSender sender, Command cmd, String label, final String[] args) {
 
@@ -28,224 +22,133 @@ public class MasterControlExecutor implements CommandExecutor {
 		if (sender instanceof Player) {
 			p = (Player) sender;
 			if (!p.hasPermission("mpc.*")) {
-				p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.general.noPerm"));
+				MasterControlMain.sendMessage(sender, MasterControlMain.main.getPLName() + MasterControlMain.main.getConfig().getString("mpc.messages.general.noPerm"));
 				return true;
 			}
 		}
 
 		if (args[0].equalsIgnoreCase("info")) {
 			if (p != null) {
-				String version = ChatColor.WHITE + main.getDescription().getVersion();
-				String author = ChatColor.WHITE + main.getDescription().getAuthors().get(0);
-				String website = ChatColor.WHITE + main.getDescription().getWebsite();
-				p.sendMessage(main.getPLName());
-				p.sendMessage(ChatColor.ITALIC + " version: " + version);
-				p.sendMessage(ChatColor.ITALIC + " by: " + author);
-				p.sendMessage(ChatColor.ITALIC + " website: " + website);
+				String version = ChatColor.WHITE + MasterControlMain.main.getDescription().getVersion();
+				String author = ChatColor.WHITE + MasterControlMain.main.getDescription().getAuthors().get(0);
+				String website = ChatColor.WHITE + MasterControlMain.main.getDescription().getWebsite();
+				MasterControlMain.sendMessage(sender, MasterControlMain.main.getPLName());
+				MasterControlMain.sendMessage(sender, ChatColor.ITALIC + " version: " + version);
+				MasterControlMain.sendMessage(sender, ChatColor.ITALIC + " by: " + author);
+				MasterControlMain.sendMessage(sender, ChatColor.ITALIC + " website: " + website);
 			} else
-				main.getLogger().info("ver: " + main.getDescription().getVersion() + " by: " + main.getDescription().getAuthors().get(0));
+				MasterControlMain.sendMessage(sender, "ver: " + MasterControlMain.main.getDescription().getVersion() + " by: " + MasterControlMain.main.getDescription().getAuthors().get(0));
 			return true;
 		}
 
 		else if (args[0].equalsIgnoreCase("help")) {
-			if (p != null) {
-				p.sendMessage(main.getPLName() + ChatColor.BOLD + "MasterPluginControl | Help");
-				p.sendMessage(ChatColor.DARK_GRAY + "/mpc help" + ChatColor.WHITE + " - " + ChatColor.ITALIC
-						+ main.getConfig().getString("mpc.messages.commands.help.help"));
-				p.sendMessage(ChatColor.DARK_GRAY + "/mpc info" + ChatColor.WHITE + " - " + ChatColor.ITALIC
-						+ main.getConfig().getString("mpc.messages.commands.help.info"));
-				p.sendMessage(ChatColor.DARK_GRAY + "/mpc reload" + ChatColor.WHITE + " - " + ChatColor.ITALIC
-						+ main.getConfig().getString("mpc.messages.commands.help.reload"));
-				p.sendMessage(ChatColor.DARK_GRAY + "/mpc list" + ChatColor.WHITE + " - " + ChatColor.ITALIC
-						+ main.getConfig().getString("mpc.messages.commands.help.list"));
-				p.sendMessage(ChatColor.DARK_GRAY + "/mpc stop <plugin>" + ChatColor.WHITE + " - " + ChatColor.ITALIC
-						+ main.getConfig().getString("mpc.messages.commands.help.stop"));
-				p.sendMessage(ChatColor.DARK_GRAY + "/mpc start <plugin>" + ChatColor.WHITE + " - " + ChatColor.ITALIC
-						+ main.getConfig().getString("mpc.messages.commands.help.start"));
-				p.sendMessage(ChatColor.DARK_GRAY + "/mpc load <plugin>" + ChatColor.WHITE + " - " + ChatColor.ITALIC
-						+ main.getConfig().getString("mpc.messages.commands.help.load"));
-				p.sendMessage(ChatColor.DARK_GRAY + "/mpc restart <plugin> [delay]" + ChatColor.WHITE + " - " + ChatColor.ITALIC
-						+ main.getConfig().getString("mpc.messages.commands.help.restart"));
-			} else {
-				main.getLogger().info("MasterPluginControl | Help");
-				main.getLogger().info("/mpc help" + " - " + main.getConfig().getString("mpc.messages.commands.help.help"));
-				main.getLogger().info("/mpc info" + " - " + main.getConfig().getString("mpc.messages.commands.help.info"));
-				main.getLogger().info("/mpc reload" + " - " + main.getConfig().getString("mpc.messages.commands.help.reload"));
-				main.getLogger().info("/mpc list" + " - " + main.getConfig().getString("mpc.messages.commands.help.list"));
-				main.getLogger().info("/mpc stop <plugin>" + " - " + main.getConfig().getString("mpc.messages.commands.help.stop"));
-				main.getLogger().info("/mpc start <plugin>" + " - " + main.getConfig().getString("mpc.messages.commands.help.start"));
-				main.getLogger().info("/mpc load <plugin>" + " - " + main.getConfig().getString("mpc.messages.commands.help.load"));
-				main.getLogger().info("/mpc restart <plugin> [delay]" + " - " + main.getConfig().getString("mpc.messages.commands.help.restart"));
+			int page = 0;
+			try {
+				page = args.length > 1 ? Integer.valueOf(args[1]) % (MasterControlMain.main.getHelp(-1).length / 8) : 0;
+			} catch (NumberFormatException e) {
+				MasterControlMain.sendMessageWithPrefix(sender, "'" + args[1] + "'" + MasterControlMain.main.getConfig().getString("mpc.messages.commands.restart.noNumber"));
 			}
+			if (p != null)
+				MasterControlMain.sendMessageWithPrefix(sender, ChatColor.BOLD + "MasterPluginControl | Help | Page " + page);
+			else
+				MasterControlMain.sendMessage(sender, "MasterPluginControl | Help | Page " + page);
+			for (String line : MasterControlMain.main.getHelp(page))
+				if (line != null)
+					MasterControlMain.sendMessage(sender, line);
 			return true;
 		}
 
 		else if (args[0].equalsIgnoreCase("reload")) {
 			if (p != null) {
-				main.loadConfig();
-				p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.general.reloadSuccessful"));
+				MasterControlMain.main.loadConfig();
+				MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.general.reloadSuccessful"));
 			} else
-				main.loadConfig();
+				MasterControlMain.main.loadConfig();
 			return true;
 		}
 
 		else if (args[0].equalsIgnoreCase("list")) {
-			if (p != null) {
-				p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.general.plugins"));
-				p.sendMessage(main.getPlugins().toString());
-			} else {
-				main.getLogger().info(main.getConfig().getString("mpc.messages.general.plugins"));
-				main.getLogger().info(main.getPlugins().toString());
-			}
+			MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.general.plugins"));
+			MasterControlMain.sendMessage(sender, MasterControlMain.main.getPlugins().toString());
 			return true;
 		}
 
 		else if (args[0].equalsIgnoreCase("stop")) {
-			if (p != null) {
-				if (args.length == 1) {
-					p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.general.plugins"));
-					p.sendMessage(main.getPlugins().toString());
-				} else {
-					if (main.getServer().getPluginManager().getPlugin(args[1]) == null)
-						p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.commands.stop.noPlugin") + "'" + args[1] + "'");
-					else {
-						final Plugin pl = main.getServer().getPluginManager().getPlugin(args[1]);
-						main.getServer().getPluginManager().disablePlugin(pl);
-						p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.commands.stop.sucessful") + "'" + args[1] + "'");
-					}
-				}
+			if (args.length == 1) {
+				MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.general.plugins"));
+				MasterControlMain.sendMessage(sender, MasterControlMain.main.getPlugins().toString());
 			} else {
-				if (args.length == 1) {
-					main.getLogger().info(main.getConfig().getString("mpc.messages.general.plugins"));
-					main.getLogger().info(main.getPlugins().toString());
-				} else {
-					if (main.getServer().getPluginManager().getPlugin(args[1]) == null)
-						main.getLogger().info(main.getConfig().getString("mpc.messages.general.noPlugin") + "'" + args[1] + "'");
-					else {
-						final Plugin pl = main.getServer().getPluginManager().getPlugin(args[1]);
-						main.getServer().getPluginManager().disablePlugin(pl);
-						main.getLogger().info(main.getConfig().getString("mpc.messages.commands.stop.sucessful") + "'" + args[1] + "'");
-					}
+				if (MasterControlMain.main.getServer().getPluginManager().getPlugin(args[1]) == null)
+					MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.commands.stop.noPlugin") + "'" + args[1] + "'");
+				else {
+					final Plugin pl = MasterControlMain.main.getServer().getPluginManager().getPlugin(args[1]);
+					MasterControlMain.main.getServer().getPluginManager().disablePlugin(pl);
+					MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.commands.stop.sucessful") + "'" + args[1] + "'");
 				}
 			}
 			return true;
 		}
 
 		else if (args[0].equalsIgnoreCase("start")) {
-			if (p != null) {
-				if (args.length == 1) {
-					p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.general.plugins"));
-					p.sendMessage(main.getPlugins().toString());
-				} else {
-					if (main.getServer().getPluginManager().getPlugin(args[1]) == null)
-						p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.general.noPlugin") + "'" + args[1] + "'");
-					else {
-						final Plugin pl = main.getServer().getPluginManager().getPlugin(args[1]);
-						main.getServer().getPluginManager().enablePlugin(pl);
-						p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.commands.start.sucessful") + "'" + args[1] + "'");
-					}
-				}
+			if (args.length == 1) {
+				MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.general.plugins"));
+				MasterControlMain.sendMessage(sender, MasterControlMain.main.getPlugins().toString());
 			} else {
-				if (args.length == 1) {
-					main.getLogger().info(main.getConfig().getString("mpc.messages.general.plugins"));
-					main.getLogger().info(main.getPlugins().toString());
-				} else {
-					if (main.getServer().getPluginManager().getPlugin(args[1]) == null)
-						main.getLogger().info(main.getConfig().getString("mpc.messages.general.noPlugin") + "'" + args[1] + "'");
-					else {
-						final Plugin pl = main.getServer().getPluginManager().getPlugin(args[1]);
-						main.getServer().getPluginManager().enablePlugin(pl);
-						main.getLogger().info(main.getConfig().getString("mpc.messages.commands.start.sucessful") + "'" + args[1] + "'");
-					}
+				if (MasterControlMain.main.getServer().getPluginManager().getPlugin(args[1]) == null)
+					MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.general.noPlugin") + "'" + args[1] + "'");
+				else {
+					final Plugin pl = MasterControlMain.main.getServer().getPluginManager().getPlugin(args[1]);
+					MasterControlMain.main.getServer().getPluginManager().enablePlugin(pl);
+					MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.commands.start.sucessful") + "'" + args[1] + "'");
 				}
 			}
 			return true;
 		}
 
 		else if (args[0].equalsIgnoreCase("load")) {
-			if (p != null) {
-				if (args.length == 1)
-					p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.commands.load.noArgs"));
-				else {
-					File pl = new File("plugins/" + args[1] + ".jar");
-					if (main.getServer().getPluginManager().getPlugin(args[1]) == null)
-						try {
-							main.getServer().getPluginManager().loadPlugin(pl);
-							main.getServer().getPluginManager().enablePlugin(main.getServer().getPluginManager().getPlugin(args[1]));
-							p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.commands.start.sucessful") + "'" + args[1] + "'");
-						} catch (Exception e) {
-							p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.general.noPlugin") + "'" + args[1] + ".jar'");
-							main.getLogger().warning(e.toString());
-						}
-					else
-						p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.commands.load.loaded"));
-				}
-			} else {
-				if (args.length == 1)
-					main.getLogger().info(main.getConfig().getString("mpc.messages.commands.load.noArgs"));
-				else {
-					File pl = new File("plugins/" + args[1] + ".jar");
-					if (main.getServer().getPluginManager().getPlugin(args[1]) == null)
-						try {
-							main.getServer().getPluginManager().loadPlugin(pl);
-							main.getServer().getPluginManager().enablePlugin(main.getServer().getPluginManager().getPlugin(args[1]));
-						} catch (Exception e) {
-							main.getLogger().warning(e.toString());
-						}
-					else
-						main.getLogger().info(main.getPLName() + main.getConfig().getString("mpc.messages.commands.load.loaded"));
-				}
+			if (args.length == 1)
+				MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.commands.load.noArgs"));
+			else {
+				File file = new File("plugins/" + args[1] + ".jar");
+				if (MasterControlMain.main.getServer().getPluginManager().getPlugin(args[1]) == null)
+					try {
+						Plugin pl = MasterControlMain.main.getServer().getPluginManager().loadPlugin(file);
+						MasterControlMain.main.getServer().getPluginManager().enablePlugin(pl);
+						MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.commands.start.sucessful") + "'" + args[1] + "'");
+					} catch (Exception e) {
+						MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.general.noPlugin") + "'" + args[1] + ".jar'");
+						MasterControlMain.main.getLogger().warning(e.toString());
+					}
+				else
+					MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.commands.load.loaded"));
 			}
 			return true;
 		}
 
 		else if (args[0].equalsIgnoreCase("restart")) {
 			int delay = 20;
-			if (args.length > 2) {
+			if (args.length > 2)
 				try {
 					delay = Integer.parseInt(args[2]);
 				} catch (NumberFormatException e) {
-					sender.sendMessage("'" + args[2] + "'" + main.getConfig().getString("mpc.messages.commands.restart.noNumber"));
+					MasterControlMain.sendMessageWithPrefix(sender, "'" + args[2] + "'" + MasterControlMain.main.getConfig().getString("mpc.messages.commands.restart.noNumber"));
 				}
-			}
-			if (p != null) {
-				if (args.length == 1) {
-					p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.general.plugins"));
-					p.sendMessage(main.getPlugins().toString());
-				} else {
-					if (main.getServer().getPluginManager().getPlugin(args[1]) == null) {
-						p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.general.noPlugin") + "'" + args[1] + "'");
-					} else {
-						final Plugin pl = main.getServer().getPluginManager().getPlugin(args[1]);
-						main.getServer().getPluginManager().disablePlugin(pl);
-						new BukkitRunnable() {
-							@Override
-							public void run() {
-								final Player p = (Player) sender;
-								main.getServer().getPluginManager().enablePlugin(pl);
-								p.sendMessage(main.getPLName() + main.getConfig().getString("mpc.messages.commands.start.sucessful") + "'" + args[1] + "'");
-							}
-						}.runTaskLater(main, delay);
-					}
-				}
+			if (args.length == 1) {
+				MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.general.plugins"));
+				MasterControlMain.sendMessage(sender, MasterControlMain.main.getPlugins().toString());
 			} else {
-				if (args.length == 1) {
-					main.getLogger().info(main.getConfig().getString("mpc.messages.general.plugins"));
-					main.getLogger().info(main.getPlugins().toString());
-				} else {
-					if (main.getServer().getPluginManager().getPlugin(args[1]) == null)
-						main.getLogger().info(main.getConfig().getString("mpc.messages.general.noPlugin") + "'" + args[1] + "'");
-					else {
-						final Plugin pl = main.getServer().getPluginManager().getPlugin(args[1]);
-						main.getServer().getPluginManager().disablePlugin(pl);
-						new BukkitRunnable() {
-							@Override
-							public void run() {
-								main.getServer().getPluginManager().enablePlugin(pl);
-								main.getLogger().info(main.getConfig().getString("mpc.messages.commands.start.sucessful") + "'" + args[1] + "'");
-							}
-						}.runTaskLater(main, delay);
-					}
+				if (MasterControlMain.main.getServer().getPluginManager().getPlugin(args[1]) == null)
+					MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.general.noPlugin") + "'" + args[1] + "'");
+				else {
+					final Plugin pl = MasterControlMain.main.getServer().getPluginManager().getPlugin(args[1]);
+					MasterControlMain.main.getServer().getPluginManager().disablePlugin(pl);
+					new BukkitRunnable() {
+						@Override
+						public void run() {
+							MasterControlMain.main.getServer().getPluginManager().enablePlugin(pl);
+							MasterControlMain.sendMessageWithPrefix(sender, MasterControlMain.main.getConfig().getString("mpc.messages.commands.start.sucessful") + "'" + args[1] + "'");
+						}
+					}.runTaskLater(MasterControlMain.main, delay);
 				}
 			}
 			return true;
--- src/de/impelon/masterplugincontrol/MasterControlMain.java
+++ src/de/impelon/masterplugincontrol/MasterControlMain.java
@@ -1,6 +1,10 @@
-package de.Impelon.masterplugincontrol;
+package de.impelon.masterplugincontrol;
+
+import java.util.Arrays;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,12 +12,14 @@ public class MasterControlMain extends JavaPlugin {
 	
 	public static MasterControlMain main;
 	private MasterControlExecutor mpcexe;
+	
+	private static String[] helppages;
 
 	@Override
 	public void onEnable() {
 		main = this;
 		loadConfig();
-		mpcexe = new MasterControlExecutor(this);
+		mpcexe = new MasterControlExecutor();
 		this.getCommand("mpc").setExecutor(mpcexe);
 	}
 
@@ -26,14 +32,16 @@ public class MasterControlMain extends JavaPlugin {
 
 	public StringBuffer getPlugins() {
 		StringBuffer sb = new StringBuffer();
-		for (Plugin plugin : this.getServer().getPluginManager().getPlugins()) {
-			sb.append(" | ");
-			if (plugin.isEnabled()) {
-				sb.append(ChatColor.DARK_GREEN + "\u269D" + ChatColor.GREEN + ChatColor.BOLD + plugin.getName() + ChatColor.WHITE);
-			} else {
-				sb.append(ChatColor.DARK_RED + "\u269D" + ChatColor.DARK_GRAY + ChatColor.ITALIC + plugin.getName() + ChatColor.WHITE);
-			}
-			sb.append(" | ");
+		int n = 0;
+		while (n < this.getServer().getPluginManager().getPlugins().length) {
+			Plugin plugin = this.getServer().getPluginManager().getPlugins()[n];
+			if (plugin.isEnabled())
+				sb.append(ChatColor.DARK_GREEN + "\u2022 " + ChatColor.GREEN + ChatColor.BOLD + plugin.getName() + ChatColor.WHITE);
+			else
+				sb.append(ChatColor.DARK_RED + "\u2022 " + ChatColor.DARK_GRAY + ChatColor.ITALIC + plugin.getName() + ChatColor.WHITE);
+			if (n <= this.getServer().getPluginManager().getPlugins().length - 1)
+				sb.append(", ");
+			n++;
 		}
 		return sb;
 	}
@@ -64,5 +72,46 @@ public class MasterControlMain extends JavaPlugin {
 
 		this.saveConfig();
 		System.out.println(this.getConfig().getString("mpc.messages.general.reloadSuccessful"));
+		
+		helppages = new String[] {
+				ChatColor.DARK_GRAY + "/mpc help <page>" + ChatColor.WHITE + " - " + ChatColor.ITALIC
+				+ this.getConfig().getString("mpc.messages.commands.help.help"),
+				ChatColor.DARK_GRAY + "/mpc info" + ChatColor.WHITE + " - " + ChatColor.ITALIC
+				+ this.getConfig().getString("mpc.messages.commands.help.info"),
+				ChatColor.DARK_GRAY + "/mpc reload" + ChatColor.WHITE + " - " + ChatColor.ITALIC
+				+ this.getConfig().getString("mpc.messages.commands.help.reload"),
+				ChatColor.DARK_GRAY + "/mpc list" + ChatColor.WHITE + " - " + ChatColor.ITALIC
+				+ this.getConfig().getString("mpc.messages.commands.help.list"),
+				ChatColor.DARK_GRAY + "/mpc stop <plugin>" + ChatColor.WHITE + " - " + ChatColor.ITALIC
+				+ this.getConfig().getString("mpc.messages.commands.help.stop"),
+				ChatColor.DARK_GRAY + "/mpc start <plugin>" + ChatColor.WHITE + " - " + ChatColor.ITALIC
+				+ this.getConfig().getString("mpc.messages.commands.help.start"),
+				ChatColor.DARK_GRAY + "/mpc load <plugin>" + ChatColor.WHITE + " - " + ChatColor.ITALIC
+				+ this.getConfig().getString("mpc.messages.commands.help.load"),
+				ChatColor.DARK_GRAY + "/mpc restart <plugin> [delay]" + ChatColor.WHITE + " - " + ChatColor.ITALIC
+				+ this.getConfig().getString("mpc.messages.commands.help.restart")
+		};
+	}
+	
+	public String[] getHelp(int page) {
+		int linesperpage = 8;
+		if (page < 0)
+			return helppages;
+		page %= helppages.length / linesperpage;
+		return Arrays.copyOfRange(helppages, page * linesperpage, (page + 1) * linesperpage);
+	}
+	
+	public static void sendMessage(CommandSender sender, String message) {
+		if (sender instanceof Player)
+			sender.sendMessage(message);
+		else
+			main.getLogger().info(message);
+	}
+	
+	public static void sendMessageWithPrefix(CommandSender sender, String message) {
+		if (sender instanceof Player)
+			sendMessage(sender, main.getPLName() + message);
+		else
+			sendMessage(sender, message);
 	}
 }
